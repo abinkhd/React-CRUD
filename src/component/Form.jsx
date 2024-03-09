26b9bab8db5b7bad8d6d18cb3516974eb528bbfb
abinkhd <abinkhd@gmail.com>
2024-03-09 20:29:59 +0530

    Implemented Update Functionality

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -12,31 +12,51 @@ const Form = () => {
 
   const [userData, setUserData] = useState([]);
   const [selectedUser, setSelectedUser] = useState({
+    userID: "",
     name: "",
     age: "",
     phone: "",
     email: "",
   });
 
+  const [isUpdate, setIsUpdate] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!user.current.name) return alert("Enter name");
-    if (!user.current.age) return alert("Enter age");
-    if (!user.current.phone) return alert("Enter Phone number");
-    if (!user.current.email) return alert("Enter Email");
-    const id = Math.ceil(Math.random() * 1000);
-    setUserData([...userData, { ...user.current, userID: id }]);
+    if (isUpdate === false) {
+      if (!user.current.name) return alert("Enter name");
+      if (!user.current.age) return alert("Enter age");
+      if (!user.current.phone) return alert("Enter Phone number");
+      if (!user.current.email) return alert("Enter Email");
+      const randomUserId = Math.ceil(Math.random() * 1000);
+      user.current.userID = randomUserId;
+      setUserData([...userData, { ...user.current }]);
+    } else {
+      setUserData(
+        userData.map((prev) =>
+          prev.userID === selectedUser.userID
+            ? {
+                ...prev,
+                ...user.current,
+              }
+            : prev
+        )
+      );
+      console.log(userData);
+    }
   };
 
   // try to handleChange get the value from event and update it to ref
   const handleOnInputChange = (e) => {
     const value = e.currentTarget.value;
     user.current[e.currentTarget.id] = value;
+    console.log(user.current);
   };
 
   const handleClick = (id) => {
+    user.current.userID = id;
     setSelectedUser(userData.find((user) => user.userID === id));
     console.log(selectedUser);
+    setIsUpdate(true);
   };
 
   return (
@@ -69,7 +89,7 @@ const Form = () => {
           onInputChange={handleOnInputChange}
         />
         <div className="form">
-          <button type="submit">Submit</button>
+          <button type="submit">{isUpdate ? "Update" : "Submit"}</button>
         </div>
       </form>
       <ul>
